481435a24e59ef06977376c064cd5acdb000b47a
MichalKorczynski <michalkorczynski0@gmail.com>
2026-07-15 21:07:27 +0200

    feat: add Task struct for Todo tasks

---
+++ src/Task.hpp
@@ -0,0 +1,11 @@
+#include <string>
+
+struct Task
+{
+    bool isCompleted = false;
+    int id;
+    int priority = 1;
+    std::string title;
+    std::string description = "-";
+    std::string dueDate = "-";
+};
